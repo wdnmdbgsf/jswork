d155350582cecdaaa6c4f1da78a2f142bc4548ed
zhoulongchuang <2780301338@qq.com>
2019-10-31 16:21:57 +0800

    work10

--- work10/index.js
+++ work10/index.js
@@ -24,11 +24,12 @@ function monkey(){
 
 function stat(){
     let str = document.getElementById("str").value
-    let obj = {};
-    if(obj[key]){
-        obj[key]++;
-    }else{
-        obj[key]=1;
-    } 
+    let obj = {}
+    let dw = str.split("")
+    dw = dw.sort()
+    for(let i = 0;i <str.length;i++){
+        let dm = dw[i]
+        obj[dm] = (obj[dm] + 1) || 1;
+    }
+    document.getElementById('result').innerText = JSON.stringify(obj)
 }
-console.log(obj);
